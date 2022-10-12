d4a34e335ed4ca4eb8d464ec69488b0b0a02df39
rharshitaaa21 <raghav.harshita21@gmail.com>
2022-10-12 11:41:59 +0530

    First Commit

---
+++ intro.js
@@ -0,0 +1,12 @@
+// serverside env
+// similar to JS
+// can connect with database
+// free and opensource 
+// chrome V8 engine is used to run Node JS
+// used for API 
+// made for using websites
+
+
+
+// client ------> (req)  server
+// server ------> (response) client
