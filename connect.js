1b0c49e4cd1418f155c03b6117ce17a05b2406b0
rharshitaaa21 <raghav.harshita21@gmail.com>
2022-10-19 01:51:33 +0530

    Website Correlation with Node

--- connect.js
+++ connect.js
@@ -1,21 +1,28 @@
-// const express =require('express');
-// const path = require('path');
-
-
-// const app = express();
-
-// const publicPath = path.join(__dirname,'public');
-// //console.log(publicPath);
-
-// app.use(express.static(publicPath)); // for accessing static files inside a folder connecting html pages and express 
-
-// app.listen(5000);
 const express = require('express');
 const path = require('path');
 
 const app = express();
 const publicPath=path.join(__dirname,'public')
 
-app.use(express.static(publicPath));
+//app.use(express.static(publicPath));
+app.get('', (req, res)=>
+{
+    res.sendFile(`${publicPath}/index.html`);
+})
+
+app.get('/about', (req, res)=>
+{
+    res.sendFile(`${publicPath}/about.html`);
+});
+app.get('/help', (req, res)=>
+{
+   res.sendFile(`${publicPath}/help.html`)
+});
+
+app.get('*', (req, res)=>
+{
+   res.sendFile(`${publicPath}/404.html`)
+});
+
 
 app.listen(5000);
