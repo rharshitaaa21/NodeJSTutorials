683168cac80a5089b6e0456c2a9e4c1a9dbdce39
rharshitaaa21 <raghav.harshita21@gmail.com>
2022-10-19 23:16:23 +0530

    Route Level Middleware

---
+++ routelevelmiddleware.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const route = express.Router(); //required to be xported only when large no of filters to be applued 100-150
+
+const reqFilter = (req,res,next)=>
+{
+    if( !req.query.age)
+    {
+        res.send("Please provide age");
+    }
+   else if( req.query.age < 18)
+    {
+        res.send("18+ restrictions");
+    }
+    else{
+        res.send("Welcome Geek")
+        next();
+    }
+}
+
+app.get('/', (res, resp) => {
+    resp.send('Welcome to Home page')
+});
+
+route.get('/users',  (res, resp) => { // use route instance whereever you need to apply middleware
+    resp.send('Welcome to Users page')
+});
+
+app.get('/contact',  (res, resp) => {
+     resp.send('Welcome to conract page')
+});
+
+route.get('/coins',  (res, resp) => {
+    resp.send('Welcome to coins page')
+});
+
+app.get('/accoutn',  (res, resp) => {
+    resp.send('Welcome to account page')
+});
+app.use('/', route); // important to instancing 
+
+app.listen(5000)
